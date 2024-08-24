1f7d2a409064c4ed1b3e94674261d5230944aba1
Joe Monkila <jnkiwa25@gmail.com>
2024-08-24 09:10:19 +0100

    training & predict

--- scripts/app.js
+++ scripts/app.js
@@ -111,7 +111,7 @@ function predictSoilFertility(model, newInput) {
     console.log(`Prédiction: ${predictedClass === 3 ? 'Fertile' : predictedClass === 2 ? 'Bientôt Fertile' : predictedClass === 1 ? 'Semi-Fertile' : 'Non Fertile'}`);
     console.log(`Années estimées pour atteindre la fertilité: ${predictedYears.toFixed(2)}`);
 
-    predictionResult.innerHTML = `Prédiction: ${predictedClass === 3 ? 'Fertile' : predictedClass === 2 ? 'Bientôt Fertile' : predictedClass === 1 ? 'Semi-Fertile' : 'Non Fertile'}<br> Années estimées pour atteindre la fertilité: ${wholeYears} ans et ${extraDays.toFixed(0)} jours`;
+    predictionResult.innerHTML = `Prédiction: ${predictedClass === 3 ? 'Fertile' : predictedClass === 2 ? 'Bientôt Fertile' : predictedClass === 1 ? 'Semi-Fertile' : 'Non Fertile'}<br> Années estimées pour atteindre la fertilité: ${wholeYears} ans <br> Jours estimées pour atteindre la fertilité ${extraDays.toFixed(0)} jours`;
 }
 
 function validateFormAndExecute(action) {
