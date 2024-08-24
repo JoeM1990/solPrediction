75615a48035b80fa1c399a3f26fe39665c9921e8
Joe Monkila <jnkiwa25@gmail.com>
2024-08-24 09:51:26 +0100

    training & predict

--- scripts/app.js
+++ scripts/app.js
@@ -103,11 +103,12 @@ function predictSoilFertility(model, newInput) {
     
     const predictedYearsFloat = predictedYearsTensor.cast('float32');
     //const predictedYears = predictedYearsFloat.dataSync()[0];
-    const predictedYears = prediction[1].dataSync()[0];
+    let predictedYears = prediction[1].dataSync()[0];
 
     if (predictedYears < 0) {
         predictedYears = 0;
     }
+    
     const wholeYears = Math.floor(predictedYears);
     const fractionalYears = predictedYears - wholeYears;
 
@@ -118,7 +119,14 @@ function predictSoilFertility(model, newInput) {
     // console.log(`Prédiction: ${predictedClass === 3 ? 'Fertile' : predictedClass === 2 ? 'Bientôt Fertile' : predictedClass === 1 ? 'Semi-Fertile' : 'Non Fertile'}`);
     // console.log(`Années estimées pour atteindre la fertilité: ${predictedYears.toFixed(2)}`);
 
-    predictionResult.innerHTML = `Prédiction: ${predictedClass === 3 ? 'Fertile' : predictedClass === 2 ? 'Bientôt Fertile' : predictedClass === 1 ? 'Semi-Fertile' : 'Non Fertile'}<br> Années estimées pour atteindre la fertilité: ${wholeYears} ans <br> Jours estimées pour atteindre la fertilité: ${extraDays.toFixed(0)} jours`;
+    if(predictedClass === 3){
+        predictionResult.innerHTML = `Prédiction: Fertile`;
+    }else if (predictedClass === 2){
+        predictionResult.innerHTML = `Prédiction: Bientôt Fertile <br> Années estimées pour atteindre la fertilité: ${wholeYears} ans <br> Jours estimées pour atteindre la fertilité: ${extraDays.toFixed(0)} jours`;
+    }else if (predictedClass === 1 || predictedClass === 0){
+        predictionResult.innerHTML = `Prédiction: Non Fertile <br> Années estimées pour atteindre la fertilité: ${wholeYears} ans <br> Jours estimées pour atteindre la fertilité: ${extraDays.toFixed(0)} jours`;
+    }
+    //predictionResult.innerHTML = `Prédiction: ${predictedClass === 3 ? 'Fertile' : predictedClass === 2 ? 'Bientôt Fertile' : predictedClass === 1 ? 'Semi-Fertile' : 'Non Fertile'}<br> Années estimées pour atteindre la fertilité: ${wholeYears} ans <br> Jours estimées pour atteindre la fertilité: ${extraDays.toFixed(0)} jours`;
 }
 
 function validateFormAndExecute(action) {
